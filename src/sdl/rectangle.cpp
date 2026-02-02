29b2c78df1b3ab70a97162f33409751c9cd50829
Connor Garey <connorgarey1999@gmail.com>
2026-02-02 18:32:43 +0000

    AMoved definitions into cpp file.

--- src/sdl/rectangle.cpp
+++ src/sdl/rectangle.cpp
@@ -3,3 +3,12 @@
 SDL::Rectangle::Rectangle(float x, float y, float w, float h)
   : rect_ {x, y, w, h} {}
 
+auto SDL::Rectangle::cref() const noexcept -> const SDL_FRect&
+{
+  return rect_;
+}
+
+auto SDL::Rectangle::ref() noexcept -> SDL_FRect&
+{
+  return rect_;
+}
