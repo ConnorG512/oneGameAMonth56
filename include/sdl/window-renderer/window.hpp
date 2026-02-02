07814f135378e85ea9b51b11fe14a83033d48d52
Connor Garey <connorgarey1999@gmail.com>
2026-02-02 19:05:02 +0000

    Set window size to 1024, and the screen unresizeable

--- include/sdl/window-renderer/window.hpp
+++ include/sdl/window-renderer/window.hpp
@@ -9,7 +9,7 @@ namespace SDL
 class Window
 {
   std::unique_ptr<SDL_Window, decltype(&SDL_DestroyWindow)> window_{
-      SDL_CreateWindow("Game Window", 1600, 900, SDL_WINDOW_RESIZABLE | SDL_WINDOW_OPENGL), &SDL_DestroyWindow};
+      SDL_CreateWindow("Game Window", 1024, 1024, SDL_WINDOW_OPENGL), &SDL_DestroyWindow};
 
 public:
   auto ptr() noexcept -> SDL_Window *;
