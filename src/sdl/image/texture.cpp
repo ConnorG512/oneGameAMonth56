5e3bb6c52b2d84623634ff3b96aafe1af9681c1d
Connor Garey <connorgarey1999@gmail.com>
2026-02-02 21:06:57 +0000

    Check for a correct path when opening a texture file.

--- src/sdl/image/texture.cpp
+++ src/sdl/image/texture.cpp
@@ -4,12 +4,16 @@
 #include <SDL3_image/SDL_image.h>
 #include <cassert>
 #include <print>
+#include <filesystem>
 
 SDL::Texture::Texture(SDL_Renderer &renderer, const char *image_path) : texture_{CreateTexture(renderer, image_path)} {}
 
 auto SDL::Texture::CreateTexture(SDL_Renderer &renderer, const char *image_path) noexcept
     -> std::unique_ptr<SDL_Texture, decltype(&SDL_DestroyTexture)>
 {
+  if(!std::filesystem::exists(image_path))
+    std::println(stderr, "Invalid file path provided!");
+
   SDL_Texture *texture{IMG_LoadTexture(&renderer, image_path)};
   if (texture == nullptr)
     std::println(stderr, "Failed to create texture! Error: [{}].", SDL_GetError());
