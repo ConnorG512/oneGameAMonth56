5dd798a9cdf8329fb8c0e75bc91d2f1cb63ac7f9
Connor Garey <connorgarey1999@gmail.com>
2026-02-03 19:20:52 +0000

    print address method used for init and display

--- src/main.cpp
+++ src/main.cpp
@@ -22,15 +22,13 @@ auto main() -> int
   };
 
   SDL::Init init{};
-  log.writeToLog(File::Logger::LogType::debug, std::format("SDLInit Address: [{}].", reinterpret_cast<void*>(&init)));
-  
+  log.writeAddress("init", static_cast<void*>(&init));
   SDL::WindowRenderer display{};
+  log.writeAddress("display", static_cast<void*>(&display));
+
   const auto window_size {display.game_window.WindowSize()};
-  log.writeToLog(File::Logger::LogType::debug, std::format("display Address: [{}].", reinterpret_cast<void*>(&display)));
   log.writeToLog(File::Logger::LogType::info, std::format("screen size: [{}*{}].", window_size.first, window_size.second));
   
-  
-
   SDL::Mouse mouse {};
 
   Gameplay::Player player({
