898631cb6292ba222ff0317f493b46e309a3698b
Connor Garey <connorgarey1999@gmail.com>
2026-02-03 18:58:47 +0000

    GetPrepend is not an anonymous namespace function so other functions can use it.

--- src/file-output/logging/logger.cpp
+++ src/file-output/logging/logger.cpp
@@ -5,6 +5,26 @@
 #include <print>
 #include <filesystem>
 
+namespace 
+{
+  auto GetPrepend(File::Logger::LogType type) -> std::string_view
+  {
+    switch (type) 
+    {
+      case File::Logger::LogType::info:
+          return "INFO:";
+        break;
+        case File::Logger::LogType::debug:
+          return "DEBUG:";
+        break;
+        case File::Logger::LogType::error:
+          return "ERROR:";
+        break;
+    }
+    return "N/A:";
+  }
+}
+
 File::Logger::Logger(const std::string& path, const std::string& file_name, bool active)
   : logfile_{std::format("{}/{}", path, file_name), std::ios::out | std::ios::trunc} 
   , active_{active}
@@ -20,21 +40,5 @@ auto File::Logger::writeToLog(LogType type, const std::string& message) noexcept
   if (!active_)
     return;
 
-  const char * prepend {nullptr};
-  
-  switch (type) 
-  {
-      case LogType::info:
-        prepend = "INFO:";
-      break;
-      case LogType::debug:
-        prepend = "DEBUG:";
-      break;
-      case LogType::error:
-        prepend = "ERROR:";
-      break;
-  }
-
-  assert(prepend != nullptr);
-  std::println(logfile_, "{} {}", prepend, message);
+  std::println(logfile_, "{} {}", GetPrepend(type), message);
 }
