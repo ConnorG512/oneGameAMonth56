1de1e6a9b3d0ab6a6b2a8e4989aa51bc7315e4c7
Connor Garey <connorgarey1999@gmail.com>
2026-02-03 20:24:26 +0000

    Created constructor

--- include/lua/lua.hpp
+++ include/lua/lua.hpp
@@ -1,13 +1,20 @@
 #pragma once 
 
+#include "file-output/logging/logger.hpp"
+
 #include <lua.hpp>
 #include <memory>
+#include <optional>
+
+namespace File {class Logger;}
 
 class LuaInstance 
 {
-  std::unique_ptr<lua_State, decltype(&lua_close)> lua_ {nullptr, &lua_close}; 
+  std::unique_ptr<lua_State, decltype(&lua_close)> lua_ {luaL_newstate(), &lua_close}; 
   
   public: 
+    LuaInstance(std::optional<std::reference_wrapper<File::Logger>> logger = std::nullopt);
+
     auto cref() const noexcept -> const lua_State&;
     auto ref() noexcept -> lua_State&;
 };
--- src/lua/lua.cpp
+++ src/lua/lua.cpp
@@ -2,6 +2,14 @@
 
 #include <cassert>
 
+LuaInstance::LuaInstance(std::optional<std::reference_wrapper<File::Logger>> logger)
+{
+  luaL_openlibs(lua_.get());
+  
+  if(logger.has_value())
+    logger->get().writeToLog(File::Logger::LogType::info, "Lua setup complete.");
+}
+
 auto LuaInstance::cref() const noexcept -> const lua_State&
 {
   assert(lua_ != nullptr);
