04cf14279b5ded0feea3f8f319bccc0dcdc7437b
Connor Garey <connorgarey1999@gmail.com>
2026-02-03 23:51:54 +0000

    Added popStack method.

--- include/lua/lua.hpp
+++ include/lua/lua.hpp
@@ -12,6 +12,8 @@ class LuaInstance
 {
   std::unique_ptr<lua_State, decltype(&lua_close)> lua_ {luaL_newstate(), &lua_close}; 
   std::optional<std::reference_wrapper<File::Logger>> logger_ {std::nullopt};
+
+  auto popStack(int num = 1) noexcept -> void;
   
   public: 
     LuaInstance(std::optional<std::reference_wrapper<File::Logger>> logger = std::nullopt);
--- src/lua/lua.cpp
+++ src/lua/lua.cpp
@@ -1,6 +1,7 @@
 #include "lua/lua.hpp"
 
 #include <cassert>
+#include <format>
 
 LuaInstance::LuaInstance(std::optional<std::reference_wrapper<File::Logger>> logger)
   : logger_ {logger}
@@ -33,6 +34,14 @@ auto LuaInstance::execFile(const char* file_name) noexcept -> void
       const auto error {lua_tostring(lua_.get(), -1)};
       logger_->get().writeToLog(
           File::Logger::LogType::error, std::format("(lua.cpp) Failed to find lua file! [{}]", error));
+      popStack();
     }
   }
+}
+
+auto LuaInstance::popStack(int num) noexcept -> void
+{
+  lua_pop(lua_.get(), num);
+  if(logger_.has_value())
+    logger_->get().writeToLog(File::Logger::LogType::debug, std::format("Lua stack popped by {}.", num));
 }
