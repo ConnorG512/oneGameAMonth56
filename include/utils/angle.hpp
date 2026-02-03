efd3a1ecc34c9915b5622ad93614e30f23c3e73e
Connor Garey <connorgarey1999@gmail.com>
2026-02-03 13:39:18 +0000

    Added floating point limitation in concept.

--- include/utils/angle.hpp
+++ include/utils/angle.hpp
@@ -2,10 +2,12 @@
 
 #include <cmath>
 #include <utility>
+#include <concepts>
 
 namespace Utils::Angle 
 {
   template <typename T>
+  requires std::floating_point<T>
   auto CaclulateAngleBetweenTwoObjects(const std::pair<T,T>& obj1, const std::pair<T,T>& obj2) -> T
   {
     return std::atan2(obj1.second - obj2.second, obj1.first - obj2.first);
