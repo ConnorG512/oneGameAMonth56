a860c6a9fb8ef3c07aab50ba8da931b18c8f417f
Connor Garey <connorgarey1999@gmail.com>
2026-02-03 19:58:26 +0000

    Made lua an RAII type.

--- include/lua/lua.hpp
+++ include/lua/lua.hpp
@@ -1,10 +1,11 @@
 #pragma once 
 
 #include <lua.hpp>
+#include <memory>
 
 class LuaInstance 
 {
-  lua_State* lua_ = luaL_newstate();
+  std::unique_ptr<lua_State, decltype(&lua_close)> lua_ {nullptr, &lua_close}; 
   
   public: 
     auto cref() const noexcept -> const lua_State&;
