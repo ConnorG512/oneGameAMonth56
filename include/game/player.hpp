c96277c707b41a7b2ff3e1caf7d8e8eae3d4e0a6
Connor Garey <connorgarey1999@gmail.com>
2026-02-06 18:56:38 +0000

    player constructor now public

--- include/game/player.hpp
+++ include/game/player.hpp
@@ -8,11 +8,12 @@ namespace Gameplay
 {
   class Player : public GameEntity
   {
-    Player(
-      const std::array<float,4>& xywh,
-      SDL_Renderer& renderer,
-      const LuaInstance& lua_instance,
-      const char* image_path
-    );
+    public:
+      Player(
+        const std::array<float,4>& xywh,
+        SDL_Renderer& renderer,
+        const LuaInstance& lua_instance,
+        const char* image_path
+      );
   };
 }
