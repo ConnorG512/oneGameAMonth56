8608b93ee5a8d7a8f179eca37c52a69b28b43c01
Connor Garey <connorgarey1999@gmail.com>
2026-02-06 18:20:38 +0000

    Added a new constructor for player.

--- include/game/player.hpp
+++ include/game/player.hpp
@@ -1,28 +1,18 @@
 #pragma once 
 
-#include "sdl/rectangle.hpp"
-#include "sdl/image/texture.hpp"
-#include "game/health.hpp"
+#include "game/game-entity.hpp"
 
 #include <array>
 
-namespace SDL 
-{
-class Renderer;
-}
 namespace Gameplay
 {
-  class Player 
+  class Player : public GameEntity
   {
-    public:
-      SDL::Rectangle collision_ {0.0, 0.0, 32.0, 32.0};
-      SDL::Texture texture_;
-      HealthComponent<int> health_{1000, 1500};
-
-      Player(
-          const std::array<float,4>& xywh,
-          SDL_Renderer& renderer,
-          const char* image_path
-      );
+    Player(
+      const std::array<float,4>& xywh,
+      SDL_Renderer& renderer,
+      const LuaInstance& lua_instance,
+      const char* image_path
+    );
   };
 }
