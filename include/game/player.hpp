d13792fe6c59c918220163de34348cf903af9cf5
Connor Garey <connorgarey1999@gmail.com>
2026-02-06 16:05:51 +0000

    Added health component to player.

--- include/game/player.hpp
+++ include/game/player.hpp
@@ -2,6 +2,7 @@
 
 #include "sdl/rectangle.hpp"
 #include "sdl/image/texture.hpp"
+#include "game/health.hpp"
 
 #include <array>
 
@@ -16,6 +17,7 @@ namespace Gameplay
     public:
       SDL::Rectangle collision_ {0.0, 0.0, 32.0, 32.0};
       SDL::Texture texture_;
+      HealthComponent<int> health_{1000, 1500};
 
       Player(
           const std::array<float,4>& xywh,
