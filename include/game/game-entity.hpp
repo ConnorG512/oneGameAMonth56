a91268c54d34c7a2dadecd78b178d28335162c0c
Connor Garey <connorgarey1999@gmail.com>
2026-02-06 18:20:23 +0000

    Added game-entity.cpp

---
+++ include/game/game-entity.hpp
@@ -0,0 +1,24 @@
+#pragma once 
+
+#include "sdl/rectangle.hpp"
+#include "sdl/image/texture.hpp"
+#include "game/health.hpp"
+#include "lua/lua.hpp"
+
+namespace Gameplay 
+{
+  class GameEntity 
+  {
+    protected: 
+      SDL::Rectangle bounds_ {0.0, 0.0, 32.0, 32.0};
+      SDL::Texture texture_;
+      HealthComponent<int> health_ {1000, 1500};
+
+      GameEntity(
+        const std::array<float,4>& xywh,
+        SDL_Renderer& renderer,
+        const LuaInstance& lua_instance,
+        const char* image_path
+      );
+  };
+}
