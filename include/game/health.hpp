faddcf9e30dbf9d677055346b48de2e2f256b5da
Connor Garey <connorgarey1999@gmail.com>
2026-02-06 15:54:50 +0000

    Moved ModifyHealth to Public

--- include/game/health.hpp
+++ include/game/health.hpp
@@ -8,25 +8,29 @@ namespace Game
     T current_ {100};
     T max_ {100};
     
-    enum class ModifyType
-    {
-      reduce,
-      increase,
-    };
-    auto ModifyHealth(ModifyType type, auto amount) noexcept -> T 
-    {
-      if(type == ModifyType::reduce)
-      {
-        current_ -= amount;
-      }
-      else if (type == ModifyType::increase)
-      {
-        current_ += amount;
-      }
-      return current_;
-    }
 
     public:
+      
+
+      enum class ModifyType
+      {
+        reduce,
+        increase,
+      };
+      auto ModifyHealth(ModifyType type, auto amount) noexcept -> T 
+      {
+        if(type == ModifyType::reduce)
+        {
+          current_ -= amount;
+        }
+        else if (type == ModifyType::increase)
+        {
+          current_ += amount;
+        }
+        return current_;
+      }
+
+
       [[nodiscard]] auto getCurrentHealth() const noexcept -> const T& 
       {
         return current_;
