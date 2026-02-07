dcf1f716671cd8b009cf72065b4090918e2b8f83
Connor Garey <connorgarey1999@gmail.com>
2026-02-07 17:05:12 +0000

    matched the same values as player.

--- gamescript/enemy.lua
+++ gamescript/enemy.lua
@@ -1,6 +1,11 @@
 GameScript = {
   Enemy = {
+    sprite = "assets/image/default.png",
     starting_health = 500,
     max_health = 600,
+    bounds_w = 32,
+    bounds_h = 32,
+    pos_x = 450,
+    pos_y = 450,
   }
 }
