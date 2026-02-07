15f41eb0806dd6a2c111bf38976e4a646f93697a
Connor Garey <connorgarey1999@gmail.com>
2026-02-07 17:43:30 +0000

    GameScript was being overrode.

--- gamescript/player.lua
+++ gamescript/player.lua
@@ -1,11 +1,9 @@
-GameScript = {
-  Player = {
-    sprite = "assets/image/player.png",
-    starting_health = 1000,
-    max_health = 1800,
-    bounds_w = 32,
-    bounds_h = 32,
-    pos_x = 150,
-    pos_y = 150,
-  }
+PlayerValues = {
+  sprite = "assets/image/player.png",
+  starting_health = 1000,
+  max_health = 1800,
+  bounds_w = 32,
+  bounds_h = 32,
+  pos_x = 150,
+  pos_y = 150,
 }
