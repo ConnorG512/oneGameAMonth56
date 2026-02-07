0bf3bd6dde07969fbcdf4b23df10982abade183d
Connor Garey <connorgarey1999@gmail.com>
2026-02-07 15:17:55 +0000

    Added extra type checks.

--- include/utils/cast-get.hpp
+++ include/utils/cast-get.hpp
@@ -2,20 +2,20 @@
 
 #include <variant>
 #include <type_traits>
+#include <string>
 
 template<typename Ret, typename T>
 auto CastGetVar(T&& vari) noexcept -> Ret
 {
-  // Read the std variant from the lua value call. 
-  // if a luanumber, cast and return to the nececery value. else return 0.
-
-  const auto visit_result {std::visit([](auto&& v) -> Ret {
-        if constexpr (std::is_floating_point_v<std::decay_t<decltype(v)>>)
+  return {std::visit([](auto&& v) -> Ret {
+        if constexpr (std::is_same_v<std::decay_t<decltype(v)>, bool>)
+          return v;
+        else if constexpr (std::is_same_v<std::decay_t<decltype(v)>, std::string>)
+          return v;
+        else if constexpr (std::is_floating_point_v<std::decay_t<decltype(v)>>)
           return static_cast<Ret>(v);
         else 
-          return static_cast<Ret>(0);
+          return {}; 
       }, vari)};
-
-  return visit_result;
 }
 
