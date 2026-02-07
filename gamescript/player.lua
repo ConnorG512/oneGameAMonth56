3a03801bff936fc46cb8a704e3cb3db5ddfd509f
Connor Garey <connorgarey1999@gmail.com>
2026-02-07 10:12:04 +0000

    Added extra values to player.lua script.

--- gamescript/player.lua
+++ gamescript/player.lua
@@ -1,6 +1,11 @@
 GameScript = {
   Player = {
+    sprite = "assets/image/player.png",
     starting_health = 1000,
     max_health = 1800,
+    bounds_w = 32,
+    bounds_h = 32,
+    pos_x = 150,
+    pos_y = 150,
   }
 }
