04500c9f6703fef798f1999fb2b0910a99a5dab0
Connor Garey <connorgarey1999@gmail.com>
2026-02-07 20:54:58 +0000

    Changed to compile time positions.

--- src/game/projectile.cpp
+++ src/game/projectile.cpp
@@ -5,10 +5,10 @@
 #include <array>
 
 Game::Projectile::Projectile(LuaInstance &lua, SDL_Renderer &renderer)
-    : bounds_{std::array{CastGetVar<float>(lua.GetLuaValue(std::array{"PlayerValues", "pos_x"})),
-                         CastGetVar<float>(lua.GetLuaValue(std::array{"PlayerValues", "pos_y"})),
-                         CastGetVar<float>(lua.GetLuaValue(std::array{"PlayerValues", "bounds_w"})),
-                         CastGetVar<float>(lua.GetLuaValue(std::array{"PlayerValues", "bounds_h"}))}},
-      texture_{renderer, CastGetVar<std::string>(lua.GetLuaValue(std::array{"PlayerValues", "sprite"})).c_str()}
+    : bounds_{std::array{600.f,
+                         600.f,
+                         CastGetVar<float>(lua.GetLuaValue(std::array{"Projectile", "bounds_w"})),
+                         CastGetVar<float>(lua.GetLuaValue(std::array{"Projectile", "bounds_h"}))}},
+      texture_{renderer, CastGetVar<std::string>(lua.GetLuaValue(std::array{"Projectile", "sprite"})).c_str()}
 {
 }
