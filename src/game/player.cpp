e3b817b7ef1bc4efec7739e8d2211777237140ad
Connor Garey <connorgarey1999@gmail.com>
2026-02-07 09:55:30 +0000

    Removed Game Entity

--- src/game/player.cpp
+++
@@ -1,9 +0,0 @@
-#include "game/player.hpp"
-
-Gameplay::Player::Player(
-      const std::array<float,4>& xywh,
-      SDL_Renderer& renderer,
-      LuaInstance& lua_instance,
-      const char* image_path
-    )
-  : GameEntity{xywh, renderer, lua_instance, image_path} {}
