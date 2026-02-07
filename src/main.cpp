9b7d9d4ceb6bd77bb67ebb1e22d99be623636c5f
Connor Garey <connorgarey1999@gmail.com>
2026-02-07 16:09:48 +0000

    Rewritten player and readded

--- src/main.cpp
+++ src/main.cpp
@@ -2,7 +2,6 @@
 #include "sdl/init.hpp"
 #include "sdl/window-renderer/window-renderer.hpp"
 #include "game/player.hpp"
-#include "game/enemy.hpp"
 #include "file-output/logging/logger.hpp"
 #include "file-output/binary/binary.hpp"
 #include "sdl/input/mouse.hpp"
@@ -47,22 +46,8 @@ auto main() -> int
   log.writeToLog(File::Logger::LogType::info, std::format("screen size: [{}*{}].", window_size.first, window_size.second));
   
   SDL::Mouse mouse {};
-
-  Gameplay::Player player({
-      static_cast<float>(window_size.first / 2 - 16), 
-      static_cast<float>(window_size.second / 2 - 16), 32.0, 32.0}, 
-      display.game_renderer.ref(), 
-      lua_instance,
-      "assets/image/player.png"
-      );
-
-  Gameplay::Enemy enemy({
-      static_cast<float>(window_size.first / 2 - 16), 
-      static_cast<float>(window_size.second / 4 - 16), 32.0, 32.0}, 
-      display.game_renderer.ref(), 
-      lua_instance,
-      "assets/image/default.png"
-      );
+  
+  Game::Player player{lua_instance, display.game_renderer.ref()};
 
   // Game loop
   while (event_handler.isGameRunning())
@@ -76,7 +61,7 @@ auto main() -> int
     display.game_renderer.renderTextureRotate(player.texture_.ref(), nullptr, &player.bounds_.ref(), 
         Utils::Angle::CaclulateAngleBetweenTwoObjectsDegree(
           mouse_xy, {player.bounds_.cref().x, player.bounds_.cref().y}) + Utils::Angle::texture_offset<double>, nullptr, SDL_FLIP_NONE);
-    display.game_renderer.renderTexture(enemy.texture_.ref(), nullptr, &enemy.bounds_.ref());
+    //display.game_renderer.renderTexture(enemy.texture_.ref(), nullptr, &enemy.bounds_.ref());
     display.game_renderer.present();
   }
 
