9d8ad18964cfd9815d9445f8e009ad0ce89f7f21
Connor Garey <connorgarey1999@gmail.com>
2026-02-07 15:00:04 +0000

    Added commnet

--- include/utils/cast-get.hpp
+++ include/utils/cast-get.hpp
@@ -6,6 +6,9 @@
 template<typename Ret, typename T>
 auto CastGetVar(T&& vari) noexcept -> Ret
 {
+  // Read the std variant from the lua value call. 
+  // if a luanumber, cast and return to the nececery value. else return 0.
+
   const auto visit_result {std::visit([](auto&& v) -> Ret {
         if constexpr (std::is_floating_point_v<std::decay_t<decltype(v)>>)
           return static_cast<Ret>(v);
