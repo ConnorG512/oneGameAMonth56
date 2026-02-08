47f94c34410c2ed0bea20c7fb6797e400ee5f68e
Connor Garey <connorgarey1999@gmail.com>
2026-02-08 22:42:37 +0000

    Added binary validation function.

--- src/file-output/binary/binary.cpp
+++ src/file-output/binary/binary.cpp
@@ -1,15 +1,34 @@
 #include "file-output/binary/binary.hpp"
 
+#include <cstring>
+#include <filesystem>
+
 File::Binary::Binary(const char *file_name)
-  : file_{file_name} 
+  : file_{file_name,  std::ios::in | std::ios::out | std::ios::binary} 
 {
   if(!file_.is_open())
-    return;
-  
-  writeToFile(magic_);
+  {
+    file_.open(file_name, std::ios::out | std::ios::binary);
+    writeToFile(magic_);
+    
+    file_.close();
+    file_.open(file_name, std::ios::in | std::ios::out | std::ios::binary);
+  }
 }
-
 auto File::Binary::writeToFile(const std::span<const uint8_t> bytes) noexcept -> void
 {
   file_.write(reinterpret_cast<const char*>(bytes.data()), bytes.size());
+}
+    
+auto File::Binary::isValidBinary() noexcept -> bool
+{
+  file_.seekg(0, std::ios::beg);
+  
+  std::array<std::uint8_t, 8> magic_from_file{};
+  file_.read(reinterpret_cast<char*>(magic_from_file.data()), 8);
+  
+  if(std::memcmp(magic_from_file.data(), magic_.data(), magic_from_file.size()) == 0)
+    return true;
+  else 
+    return false;
 }
