fae8a4a99909ec52e63330e94b0b5ebfd97f49f8
Connor Garey <connorgarey1999@gmail.com>
2026-02-08 17:10:32 +0000

    remove const from loop and passed in correct parameters.

--- include/game/spawner.hpp
+++ include/game/spawner.hpp
@@ -5,6 +5,10 @@
 #include <memory>
 #include <functional>
 #include <ranges>
+#include <utility>
+
+class LuaInstance;
+class SDL_Renderer;
 
 namespace Game {
 template <typename T>
@@ -25,13 +29,13 @@ class Spawner
     Spawner(std::function<std::uint32_t()> slot_func)
       : spawn_slots_{CreateSpawner(slot_func)} {}
 
-    auto spawnProjectile() -> void 
+    auto spawnProjectile(const std::pair<float, float> &xy, LuaInstance &lua, SDL_Renderer &renderer) -> void 
     {
-      for(const auto& slot : spawn_slots_)
+      for(auto& slot : spawn_slots_)
       {
         if(slot == nullptr)
         {
-          slot = std::make_unique<T>();
+          slot = std::make_unique<T>(xy, lua, renderer);
           break;
         }
       }
