9d0dc65c6e3599be841942a632e0b35929569b5c
Connor Garey <connorgarey1999@gmail.com>
2026-02-09 18:49:32 +0000

    added procedual ttf example

--- src/main.cpp
+++ src/main.cpp
@@ -15,10 +15,15 @@
 #include "sdl/window-renderer/window-renderer.hpp"
 #include "utils/angle.hpp"
 #include "utils/cast-get.hpp"
-#include "sdl/ttf.hpp"
 
+#include <SDL3/SDL_error.h>
+#include <SDL3/SDL_rect.h>
+#include <SDL3/SDL_render.h>
+#include <SDL3/SDL_surface.h>
 #include <array>
 #include <format>
+#include <SDL3_ttf/SDL_ttf.h>
+#include <print>
 
 auto main() -> int
 {
@@ -42,11 +47,16 @@ auto main() -> int
 
   SDL::Init init{};
   log.writeAddress("init", static_cast<void *>(&init));
-
-  SDL::Ttf ttf{};
+  
+  // Ttf
+  auto font {TTF_OpenFont("assets/fonts/Orbitron/static/Orbitron-Regular/ttf", 32)};
+  if (font == nullptr)
+  {
+    std::println("Failed to get font! {}", SDL_GetError());
+    return -1;
+  }
 
   SDL::EventHandler event_handler{log};
-
   SDL::WindowRenderer display{
       "Game window",
       {std::get<double>(lua_instance.GetLuaValue(std::array{"AppConfiguration", "Display", "Resolution", "x"})),
@@ -75,6 +85,17 @@ auto main() -> int
     event_handler.PollEvent([&] { proj_spawner.spawnProjectile(mouse_xy, lua_instance, display.game_renderer.ref()); });
 
     display.game_renderer.clearScreen();
+    
+    //ttf 
+    auto surface {TTF_RenderText_Blended(font, "Player", 0, {255, 255, 255, 255})};
+    auto texture {SDL_CreateTextureFromSurface(&display.game_renderer.ref(), surface)};
+    auto texture_ref {*texture};
+    SDL_DestroySurface(surface);
+    SDL_FRect dstRect {100, 100, 200, 80};
+    display.game_renderer.renderTexture(texture_ref, nullptr, &dstRect);
+    SDL_DestroyTexture(texture);
+
+
     display.game_renderer.drawColorFloat(0, 0, 0);
     display.game_renderer.renderTextureRotate(player.texture_.ref(), nullptr, &player.bounds_.ref(),
                                               Utils::Angle::CaclulateAngleBetweenTwoObjectsDegree(
