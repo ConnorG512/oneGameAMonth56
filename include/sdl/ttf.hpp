03e331c99d4cd9b405751ca69b046f999b1d43b9
Connor Garey <connorgarey1999@gmail.com>
2026-02-09 17:55:15 +0000

    Added unqiue ptr and ref get.

--- include/sdl/ttf.hpp
+++ include/sdl/ttf.hpp
@@ -1,13 +1,25 @@
 #pragma once 
 
 #include <SDL3_ttf/SDL_ttf.h>
+#include <cassert>
+#include <memory>
+
+class SDL_Renderer; 
 
 namespace SDL 
 {
   class Ttf 
   {
+    std::unique_ptr<TTF_Font, decltype(&TTF_CloseFont)> font_ {nullptr, &TTF_CloseFont}; 
+
     public:
       Ttf();
       ~Ttf();
+
+      auto ref() noexcept -> TTF_Font& 
+      {
+        assert(font_.get() != nullptr);
+        return *font_.get();
+      }
   };
 }
