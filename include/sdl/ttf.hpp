d2d59c876359bc76cf40ecdf548a521963c15e46
Connor Garey <connorgarey1999@gmail.com>
2026-02-09 19:54:20 +0000

    Created a CreateFont method and constructor.

--- include/sdl/ttf.hpp
+++ include/sdl/ttf.hpp
@@ -1,25 +1,27 @@
-#pragma once 
+#pragma once
 
 #include <SDL3_ttf/SDL_ttf.h>
 #include <cassert>
 #include <memory>
 
-class SDL_Renderer; 
+class SDL_Renderer;
 
-namespace SDL 
+namespace SDL
 {
-  class Ttf 
+class Ttf
+{
+  std::unique_ptr<TTF_Font, decltype(&TTF_CloseFont)> font_{nullptr, &TTF_CloseFont};
+
+  auto CreateFont(const char *font_path, float size) -> std::unique_ptr<TTF_Font, decltype(&TTF_CloseFont)>;
+
+public:
+  Ttf(const char *font_path, float size);
+  ~Ttf() = default;
+
+  auto ref() noexcept -> TTF_Font &
   {
-    std::unique_ptr<TTF_Font, decltype(&TTF_CloseFont)> font_ {nullptr, &TTF_CloseFont}; 
-
-    public:
-      Ttf();
-      ~Ttf();
-
-      auto ref() noexcept -> TTF_Font& 
-      {
-        assert(font_.get() != nullptr);
-        return *font_.get();
-      }
-  };
-}
+    assert(font_.get() != nullptr);
+    return *font_.get();
+  }
+};
+} // namespace SDL
--- src/sdl/ttf.cpp
+++ src/sdl/ttf.cpp
@@ -1,15 +1,19 @@
 #include "sdl/ttf.hpp"
 
+#include <SDL3/SDL_error.h>
+#include <SDL3_ttf/SDL_ttf.h>
+#include <format>
 #include <stdexcept>
 
-SDL::Ttf::Ttf() 
-{
-  if(!TTF_Init())
-    throw std::runtime_error("Failed to init SDL ttf!");
-}
+SDL::Ttf::Ttf(const char *font_path, float size) : font_{CreateFont(font_path, size)} {}
 
-SDL::Ttf::~Ttf() 
+auto SDL::Ttf::CreateFont(const char *font_path, float size) -> std::unique_ptr<TTF_Font, decltype(&TTF_CloseFont)>
 {
-  TTF_Quit();
-}
+  TTF_Font *created_font{TTF_OpenFont(font_path, size)};
+  if (created_font == nullptr)
+  {
+    throw std::runtime_error(std::format("Failed to create font! {}", SDL_GetError()));
+  }
 
+  return {created_font, &TTF_CloseFont};
+}
