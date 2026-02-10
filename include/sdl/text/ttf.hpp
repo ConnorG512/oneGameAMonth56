647cb4c00c7ed122df28aad3a3e561f0df30cc06
Connor Garey <connorgarey1999@gmail.com>
2026-02-10 21:59:38 +0000

    Changed parameters for ttf methods

--- include/sdl/text/ttf.hpp
+++ include/sdl/text/ttf.hpp
@@ -2,9 +2,11 @@
 
 #include <SDL3_ttf/SDL_ttf.h>
 #include <cassert>
+#include <cstddef>
 #include <memory>
 
 class SDL_Renderer;
+class SDL_Surface;
 
 namespace SDL
 {
@@ -12,10 +14,11 @@ class Ttf
 {
   std::unique_ptr<TTF_Font, decltype(&TTF_CloseFont)> font_{nullptr, &TTF_CloseFont};
 
-  auto CreateFont(const char *font_path, float size) -> std::unique_ptr<TTF_Font, decltype(&TTF_CloseFont)>;
+  auto OpenFont(const char *font_path, float size) -> std::unique_ptr<TTF_Font, decltype(&TTF_CloseFont)>;
+  auto CreateText(const char* text, std::size_t len) -> SDL_Surface*;
 
 public:
-  Ttf(const char *font_path, float size);
+  Ttf(const char* font_path, float size);
   ~Ttf() = default;
 
   auto ref() noexcept -> TTF_Font &
--- src/sdl/text/ttf.cpp
+++ src/sdl/text/ttf.cpp
@@ -1,13 +1,15 @@
 #include "sdl/text/ttf.hpp"
 
 #include <SDL3/SDL_error.h>
+#include <SDL3/SDL_surface.h>
 #include <SDL3_ttf/SDL_ttf.h>
 #include <format>
+#include <memory>
 #include <stdexcept>
 
-SDL::Ttf::Ttf(const char *font_path, float size) : font_{CreateFont(font_path, size)} {}
+SDL::Ttf::Ttf(const char *font_path, float size) : font_{OpenFont(font_path, size)} {}
 
-auto SDL::Ttf::CreateFont(const char *font_path, float size) -> std::unique_ptr<TTF_Font, decltype(&TTF_CloseFont)>
+auto SDL::Ttf::OpenFont(const char *font_path, float size) -> std::unique_ptr<TTF_Font, decltype(&TTF_CloseFont)>
 {
   TTF_Font *created_font{TTF_OpenFont(font_path, size)};
   if (created_font == nullptr)
