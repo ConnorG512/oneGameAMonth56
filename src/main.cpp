8520d692b8f3100c9c46cbd0ac5a2f3aecc8418a
Connor Garey <connorgarey1999@gmail.com>
2026-02-10 22:03:19 +0000

    text is rendering to the corner of the screen.

--- src/main.cpp
+++ src/main.cpp
@@ -12,6 +12,7 @@
 #include "sdl/image/texture.hpp"
 #include "sdl/init.hpp"
 #include "sdl/input/mouse.hpp"
+#include "sdl/text/text.hpp"
 #include "sdl/window-renderer/window-renderer.hpp"
 #include "utils/angle.hpp"
 #include "utils/cast-get.hpp"
@@ -20,10 +21,9 @@
 #include <SDL3/SDL_rect.h>
 #include <SDL3/SDL_render.h>
 #include <SDL3/SDL_surface.h>
+#include <SDL3_ttf/SDL_ttf.h>
 #include <array>
 #include <format>
-#include <SDL3_ttf/SDL_ttf.h>
-#include <print>
 
 auto main() -> int
 {
@@ -47,14 +47,6 @@ auto main() -> int
 
   SDL::Init init{};
   log.writeAddress("init", static_cast<void *>(&init));
-  
-  // Ttf
-  auto font {TTF_OpenFont("assets/fonts/Orbitron/static/Orbitron-Regular.ttf", 32)};
-  if (font == nullptr)
-  {
-    std::println("Failed to get font! {}", SDL_GetError());
-    return -1;
-  }
 
   SDL::EventHandler event_handler{log};
   SDL::WindowRenderer display{
@@ -65,6 +57,9 @@ auto main() -> int
   const auto window_size{display.game_window.WindowSize()};
   log.writeToLog(File::Logger::LogType::info,
                  std::format("screen size: [{}*{}].", window_size.first, window_size.second));
+
+  SDL::Text score_text{
+      32, {20, 20, 100, 20}, "Player", 12, display.game_renderer.ref()};
 
   SDL::Mouse mouse{};
 
@@ -85,16 +80,6 @@ auto main() -> int
     event_handler.PollEvent([&] { proj_spawner.spawnProjectile(mouse_xy, lua_instance, display.game_renderer.ref()); });
 
     display.game_renderer.clearScreen();
-    
-    //ttf 
-    auto surface {TTF_RenderText_Blended(font, "Player", 0, {255, 255, 255, 255})};
-    auto texture {SDL_CreateTextureFromSurface(&display.game_renderer.ref(), surface)};
-    auto texture_ref {*texture};
-    SDL_DestroySurface(surface);
-    SDL_FRect dstRect {100, 100, 200, 80};
-    SDL_DestroyTexture(texture);
-
-
     display.game_renderer.drawColorFloat(0, 0, 0);
     display.game_renderer.renderTextureRotate(player.texture_.ref(), nullptr, &player.bounds_.ref(),
                                               Utils::Angle::CaclulateAngleBetweenTwoObjectsDegree(
@@ -103,9 +88,7 @@ auto main() -> int
                                               nullptr, SDL_FLIP_NONE);
     display.game_renderer.renderTexture(enemy.texture_.ref(), nullptr, &enemy.bounds_.ref());
     
-    //ttf
-    display.game_renderer.renderTexture(texture_ref, nullptr, &dstRect);
-
+    display.game_renderer.renderTexture(score_text.texture.ref(), nullptr, &score_text.rectangle.ref());
     // Spawned Projectile Render:
     for (auto &projectile : proj_spawner.ref())
     {
@@ -113,6 +96,7 @@ auto main() -> int
     }
 
     display.game_renderer.present();
+    //SDL_DestroyTexture(texture);
   }
 
   file_data.high_score = current_score.getHighScore();
