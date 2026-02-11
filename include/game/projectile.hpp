fb80ec4a2bcf3ab652f2c22dffa58ebf58698cdb
Connor Garey <connorgarey1999@gmail.com>
2026-02-11 21:03:31 +0000

    screen bounds instead goes through contructor and not a parameter.

--- include/game/projectile.hpp
+++ include/game/projectile.hpp
@@ -12,12 +12,14 @@ namespace Game
 {
 class Projectile
 {
+  std::pair<int, int> screen_max_xy_{0, 0};
+
 public:
   SDL::Rectangle bounds_{};
   SDL::Texture texture_;
 
-  auto destroy(std::pair<int, int>max_screen_bounds) -> bool;
+  auto destroy() -> bool;
 
-  Projectile(std::pair<float, float> xy, LuaInstance &lua, SDL_Renderer &renderer);
+  Projectile(std::pair<int, int> screen_max_xy, std::pair<float, float> xy, LuaInstance &lua, SDL_Renderer &renderer);
 };
 } // namespace Game
--- src/game/projectile.cpp
+++ src/game/projectile.cpp
@@ -3,22 +3,25 @@
 #include "utils/cast-get.hpp"
 
 #include <array>
+#include <utility>
 
-Game::Projectile::Projectile(std::pair<float, float> xy, LuaInstance &lua, SDL_Renderer &renderer)
-    : bounds_{std::array{xy.first, xy.second, CastGetVar<float>(lua.GetLuaValue(std::array{"Projectile", "bounds_w"})),
+Game::Projectile::Projectile(std::pair<int, int> screen_max_xy, std::pair<float, float> xy, LuaInstance &lua,
+                             SDL_Renderer &renderer)
+    : screen_max_xy_{screen_max_xy},
+      bounds_{std::array{xy.first, xy.second, CastGetVar<float>(lua.GetLuaValue(std::array{"Projectile", "bounds_w"})),
                          CastGetVar<float>(lua.GetLuaValue(std::array{"Projectile", "bounds_h"}))}},
       texture_{renderer, CastGetVar<std::string>(lua.GetLuaValue(std::array{"Projectile", "sprite"})).c_str()}
 {
 }
 
-auto Game::Projectile::destroy(std::pair<int, int>max_screen_bounds) -> bool
+auto Game::Projectile::destroy() -> bool
 {
-  auto [screen_width, screen_height] = max_screen_bounds;
+  auto [screen_width, screen_height] = screen_max_xy_;
   if (bounds_.cref().h > screen_height || bounds_.ref().w > screen_width)
   {
     return true;
   }
-  
+
   if (bounds_.cref().h < 0 || bounds_.ref().w < 0)
   {
     return true;
