541408746c17ad75778608d79f5de13e660016d9
Connor Garey <connorgarey1999@gmail.com>
2026-02-11 14:58:45 +0000

    Added Radians to end of fuction.

--- include/utils/angle.hpp
+++ include/utils/angle.hpp
@@ -13,7 +13,7 @@ constexpr T texture_offset{90.0};
 
 template <typename T>
   requires std::floating_point<T>
-auto CaclulateAngleBetweenTwoObjects(const std::pair<T, T> &obj1, const std::pair<T, T> &obj2) -> T
+auto CaclulateAngleBetweenTwoObjectsRadians(const std::pair<T, T> &obj1, const std::pair<T, T> &obj2) -> T
 {
   return std::atan2(obj1.second - obj2.second, obj1.first - obj2.first);
 }
