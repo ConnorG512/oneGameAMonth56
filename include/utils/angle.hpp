f7c6bce5e17b51298755e7584d9f65d89e6ca9e1
Connor Garey <connorgarey1999@gmail.com>
2026-02-11 15:07:01 +0000

    replaced the requires with a single concept.

--- include/utils/angle.hpp
+++ include/utils/angle.hpp
@@ -8,21 +8,21 @@
 namespace Utils::Angle
 {
 template <typename T>
-  requires std::floating_point<T>
-constexpr T texture_offset{90.0};
+concept Number = std::integral<T> && !std::same_as<T, bool>;
 
-template <typename T>
-  requires std::floating_point<T>
+template <Number T> constexpr T texture_offset{90.0};
+
+template <Number T>
 auto CaclulateAngleBetweenTwoObjectsRadians(const std::pair<T, T> &obj1, const std::pair<T, T> &obj2) -> T
 {
   return std::atan2(obj1.second - obj2.second, obj1.first - obj2.first);
 }
 
-template <typename T>
-  requires std::floating_point<T>
+template <Number T>
 auto CaclulateAngleBetweenTwoObjectsDegree(const std::pair<T, T> &obj1, const std::pair<T, T> &obj2) -> T
 {
   const auto radians{std::atan2(obj1.second - obj2.second, obj1.first - obj2.first)};
   return radians * (180 / std::numbers::pi_v<T>);
 }
+
 } // namespace Utils::Angle
