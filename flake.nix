61645cebab7c4db40a1354cb9b6277d67cbf80fc
Connor Garey <connorgarey1999@gmail.com>
2026-02-11 23:16:48 +0000

    Added Release derivation.

--- flake.nix
+++ flake.nix
@@ -8,6 +8,7 @@
   outputs = { self, nixpkgs }: 
   let
     pkgs = nixpkgs.legacyPackages.x86_64-linux;
+    lib = nixpkgs.lib;
   in 
   {
     devShells.x86_64-linux.default = pkgs.mkShell {
@@ -36,6 +37,42 @@
 
       cmakeFlags = [
         "-DCMAKE_BUILD_TYPE=Debug"
+      ];
+
+      nativeBuildInputs = with pkgs; [
+        makeWrapper
+        cmake 
+        ninja
+        pkg-config
+        
+      ];
+      buildInputs = with pkgs; [
+        sdl3
+        sdl3-image
+        sdl3-ttf
+        mesa
+        
+        lua
+      ];
+      
+      # Missing libGL.so.1 runtime error. Wrapping to fix
+      postFixup = ''
+        wrapProgram $out/bin/oneGameAMonth \
+          --set LD_LIBRARY_PATH ${lib.makeLibraryPath [
+            pkgs.libGL.out
+          ]}
+      '';
+    });
+    
+    release = pkgs.stdenv.mkDerivation (finalAttrs: {
+      pname = "oneGameAMonth";
+      version = "debug";
+      src = ./.;
+
+      dontStrip = true;
+
+      cmakeFlags = [
+        "-DCMAKE_BUILD_TYPE=Release"
       ];
 
       nativeBuildInputs = with pkgs; [
