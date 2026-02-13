993543ec5e4f2446d4269ad4625744010473c2f9
Connor Garey <connorgarey1999@gmail.com>
2026-02-13 22:03:27 +0000

    Created Text class.

--- include/sdl/text/text.hpp
+++ include/sdl/text/text.hpp
@@ -1,3 +1,32 @@
 #pragma once
 
+#include <SDL3_ttf/SDL_ttf.h>
+#include <memory>
+#include <utility>
 
+// Forward
+class SDL_Renderer;
+//
+
+namespace SDL
+{
+class Text
+{
+  SDL_Renderer &renderer_;
+
+  std::unique_ptr<TTF_TextEngine, decltype(&TTF_DestroyRendererTextEngine)> text_engine_{
+      TTF_CreateRendererTextEngine(&renderer_), &TTF_DestroyRendererTextEngine};
+
+  std::unique_ptr<TTF_Font, decltype(&TTF_CloseFont)> font_{
+      TTF_OpenFont("assets/fonts/Orbitron/static/Orbitron-Regular.ttf", 2), &TTF_CloseFont};
+
+  std::unique_ptr<TTF_Text, decltype(&TTF_DestroyText)> text_object_{
+      TTF_CreateText(text_engine_.get(), font_.get(), "Text", 32), &TTF_DestroyText};
+
+public:
+  Text(const std::string&, SDL_Renderer &renderer);
+
+  auto draw(std::pair<float, float>xy) noexcept -> void;
+  auto swapText(const std::string& text);
+};
+} // namespace SDL
--- src/sdl/text/text.cpp
+++ src/sdl/text/text.cpp
@@ -1,1 +1,16 @@
+#include "sdl/text/text.hpp"
 
+SDL::Text::Text(const std::string& text, SDL_Renderer &renderer)
+    : renderer_{renderer}, text_object_{TTF_CreateText(text_engine_.get(), font_.get(), text.c_str(), text.size()), &TTF_DestroyText}
+{
+}
+
+auto SDL::Text::draw(std::pair<float, float>xy) noexcept -> void
+{
+  TTF_DrawRendererText(text_object_.get(), xy.first, xy.second);
+}
+
+auto SDL::Text::swapText(const std::string& text)
+{
+  TTF_SetTextString(text_object_.get(), text.c_str(), text.size());
+}
