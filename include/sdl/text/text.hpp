aae29117b3c13a65028d587038706bcf8a4433f3
Connor Garey <connorgarey1999@gmail.com>
2026-02-13 19:46:07 +0000

    Removed old content

--- include/sdl/text/text.hpp
+++ include/sdl/text/text.hpp
@@ -1,21 +1,3 @@
 #pragma once
 
-#include "sdl/image/texture.hpp"
-#include "sdl/rectangle.hpp"
-#include "sdl/text/ttf.hpp"
 
-#include <array>
-
-class SDL_Renderer;
-
-namespace SDL
-{
-struct Text
-{
-  Text(float text_size, std::array<float, 4> bounds_xywh, const char* text, size_t len, SDL_Renderer &renderer);
-
-  SDL::Ttf ttf;
-  SDL::Texture texture;
-  SDL::Rectangle rectangle{32, 32, 32, 32};
-};
-} // namespace SDL
--- src/sdl/text/text.cpp
+++ src/sdl/text/text.cpp
@@ -1,6 +1,1 @@
-#include "sdl/text/text.hpp"
 
-SDL::Text::Text(float text_size, std::array<float, 4> bounds_xywh, const char* text, size_t len, SDL_Renderer &renderer)
-    : ttf{"assets/fonts/Orbitron/static/Orbitron-Regular.ttf", text_size}
-    , texture{renderer, ttf.ref(), text, len}
-    , rectangle{bounds_xywh} {}
