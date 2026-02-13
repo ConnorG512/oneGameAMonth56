876a70384cb7deffdb7b008a9318bab5e3d630ca
Connor Garey <connorgarey1999@gmail.com>
2026-02-13 14:55:36 +0000

    Added Score to player.

--- include/game/player.hpp
+++ include/game/player.hpp
@@ -3,6 +3,7 @@
 #include "game/health.hpp"
 #include "sdl/image/texture.hpp"
 #include "sdl/rectangle.hpp"
+#include "game/counter.hpp"
 
 class LuaInstance;
 namespace File
@@ -19,6 +20,7 @@ public:
   SDL::Rectangle bounds_{};
   SDL::Texture texture_;
   Game::HealthComponent<int> health_{};
+  Game::Counter<int> score_{1000000};
 
   Player(LuaInstance &lua, SDL_Renderer &renderer);
 };
