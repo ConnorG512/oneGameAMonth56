cb3da7848d838b595a3d597d39037c031f57062a
Connor Garey <connorgarey1999@gmail.com>
2026-02-13 14:15:24 +0000

    Replaced FloatingPoint Concept with the in built variant.

--- include/utils/angle.hpp
+++ include/utils/angle.hpp
@@ -7,20 +7,17 @@
 
 namespace Utils::Angle
 {
-template <typename T>
-concept FloatingPoint = std::floating_point<T>;
-
 // SDL sets neutral not facing upward. constant texture offset for making the sprite point the correct way.
-template <FloatingPoint T> constexpr T texture_offset{90.0};
+template <std::floating_point T> constexpr T texture_offset{90.0};
 
 // Used to calculate the angle between to objects so that they face toward eatchother. 
-template <FloatingPoint T>
+template <std::floating_point T>
 auto CaclulateAngleBetweenTwoObjectsRadians(const std::pair<T, T> &obj1, const std::pair<T, T> &obj2) -> T
 {
   return std::atan2(obj1.second - obj2.second, obj1.first - obj2.first);
 }
 
-template <FloatingPoint T>
+template <std::floating_point T>
 auto CaclulateAngleBetweenTwoObjectsDegree(const std::pair<T, T> &obj1, const std::pair<T, T> &obj2) -> T
 {
   const auto radians{std::atan2(obj1.second - obj2.second, obj1.first - obj2.first)};
@@ -28,13 +25,13 @@ auto CaclulateAngleBetweenTwoObjectsDegree(const std::pair<T, T> &obj1, const st
 }
 
 // Move in the correct angle given.
-template <FloatingPoint T>
+template <std::floating_point T>
 auto CalculateYDirection(T radians, T speed) -> T
 {
   return std::sin(radians) * speed;
 }
 
-template <FloatingPoint T>
+template <std::floating_point T>
 auto CalculateXDirection(T radians, T speed) -> T
 {
   return std::cos(radians) * speed;
