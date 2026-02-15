ea9287587943264db7f1dd0e9b0526430a1529b7
Connor Garey <connorgarey1999@gmail.com>
2026-02-15 21:36:54 +0000

    Added Audio init

--- src/sdl/init.cpp
+++ src/sdl/init.cpp
@@ -1,6 +1,7 @@
 #include "sdl/init.hpp"
 
 #include <SDL3/SDL.h>
+#include <SDL3/SDL_init.h>
 #include <SDL3_ttf/SDL_ttf.h>
 #include <format>
 #include <stdexcept>
@@ -11,6 +12,8 @@ SDL::Init::Init()
     throw std::runtime_error(std::format("Could not initialise SDL! Error: [].", SDL_GetError()).c_str());
   if (!TTF_Init())
     throw std::runtime_error(std::format("Could not get sdl3 ttf! Error: [].", SDL_GetError()).c_str());
+  if(SDL_Init(SDL_INIT_AUDIO))
+    throw std::runtime_error(std::format("Could not initialise SDL Audio! Error: [].", SDL_GetError()).c_str());
 }
 
 SDL::Init::~Init()
