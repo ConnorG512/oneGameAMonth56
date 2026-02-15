a359dd2ddcfddae970a60b271e983d985230d85b
Connor Garey <connorgarey1999@gmail.com>
2026-02-15 15:17:30 +0000

    Added a constructor for projectile.

--- src/game/projectile.cpp
+++ src/game/projectile.cpp
@@ -14,6 +14,18 @@ Game::Projectile::Projectile(std::pair<int, int> screen_max_xy, std::pair<float,
 {
 }
 
+Game::Projectile::Projectile(std::pair<int, int> screen_max_xy, std::pair<float, float> xy,
+                             std::pair<float, float> vel_xy, LuaInstance &lua, SDL_Renderer &renderer)
+    : screen_max_xy_{screen_max_xy}, bounds_{xy.first,
+                                             xy.second,
+                                             CastGetVar<float>(lua.GetLuaValue(std::array{"Projectile", "bounds_w"})),
+                                             CastGetVar<float>(lua.GetLuaValue(std::array{"Projectile", "bounds_h"})),
+                                             vel_xy.first,
+                                             vel_xy.second},
+      texture_{renderer, CastGetVar<std::string>(lua.GetLuaValue(std::array{"Projectile", "sprite"})).c_str()}
+{
+}
+
 auto Game::Projectile::isReadyToBeDestroyed() -> bool
 {
   auto [screen_width, screen_height] = screen_max_xy_;
