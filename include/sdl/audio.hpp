62acc758b01e4cf81416afb87866d9cc45706de1
Connor Garey <connorgarey1999@gmail.com>
2026-02-16 21:24:32 +0000

    Added spec
    .

--- include/sdl/audio.hpp
+++ include/sdl/audio.hpp
@@ -8,8 +8,14 @@ namespace SDL
 {
 class Audio
 {
+  SDL_AudioSpec spec{
+      .format = SDL_AUDIO_F32,
+      .channels = 2,
+      .freq = 48000,
+  };
+
   std::unique_ptr<SDL_AudioStream, decltype(&SDL_DestroyAudioStream)> audio_stream_{
-      SDL_OpenAudioDeviceStream(SDL_AUDIO_DEVICE_DEFAULT_PLAYBACK, 0, nullptr, nullptr), &SDL_DestroyAudioStream};
+      SDL_OpenAudioDeviceStream(SDL_AUDIO_DEVICE_DEFAULT_PLAYBACK, &spec, nullptr, nullptr), &SDL_DestroyAudioStream};
 
 public:
   auto pushStream(std::span<const float> buffer) -> void;
