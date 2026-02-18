d36aa9307b0e1d6042af20b511bc60ae3ff40ecd
Connor Garey <connorgarey1999@gmail.com>
2026-02-18 16:29:52 +0000

    Using UI layout constructor.

--- src/main.cpp
+++ src/main.cpp
@@ -59,111 +59,121 @@ auto main() -> int
   SDL::Audio audio{};
   audio.getAudioDevice();
 
-  UI::Layout<int> ui{};
-
-  SDL::Text player_text{CastGetVar<std::string>(lua_instance.GetLuaValue(std::array{"PlayerValues", "name"})),
-                        display.game_renderer.ref()};
-  SDL::Text score_text{std::format("{:09}", 0), display.game_renderer.ref()};
-  SDL::Text high_score_text{std::format("{:09}", file_data.high_score).c_str(), display.game_renderer.ref()};
-  SDL::Text times_played_text{std::format("{:0}", file_data.times_played).c_str(), display.game_renderer.ref()};
-
-  SDL::Mouse mouse{};
-
-  Utils::Rng rng{};
-
-  Game::Rules game_rules{CastGetVar<int>(lua_instance.GetLuaValue(std::array{"GameRules", "max_time"})),
-                         CastGetVar<int>(lua_instance.GetLuaValue(std::array{"GameRules", "max_score"}))};
-
-  Game::Player player{lua_instance, display.game_renderer.ref()};
-  Game::Spawner<Game::Projectile> proj_spawner{[&lua_instance]()
-                                               {
-                                                 return CastGetVar<int>(lua_instance.GetLuaValue(
-                                                     std::array{"GameRules", "ProjectileSpawner", "max_spawn_slots"}));
-                                               }};
-  Game::Spawner<Game::Enemy> enemy_spawner{};
-
-  // Game loop
-  while (event_handler.isGameRunning())
-  {
-
-    const auto mouse_xy{mouse.GetCursorPosition()};
-    const auto mouse_radian{Utils::Angle::CaclulateAngleBetweenTwoObjectsRadians(
-        mouse_xy, {player.bounds_.cref().x, player.bounds_.cref().y})};
-
-    event_handler.PollEvent(
-        [&]
-        {
-          proj_spawner.spawn(
-              display.game_window.WindowSize(),
-              std::pair<float, float>{display.game_window.WindowSize().first / 2 - 16,
-                                      display.game_window.WindowSize().second / 2 - 16},
-              std::pair<float, float>{
-                  Utils::Angle::CalculateXDirection(
-                      mouse_radian, CastGetVar<float>(lua_instance.GetLuaValue(std::array{"Projectile", "speed"}))),
-                  Utils::Angle::CalculateYDirection(
-                      mouse_radian, CastGetVar<float>(lua_instance.GetLuaValue(std::array{"Projectile", "speed"})))},
-              lua_instance, display.game_renderer.ref());
-        });
-
-    proj_spawner.clearSlot();
-
-    enemy_spawner.spawn(static_cast<float>(rng.generate(0, 1024)), static_cast<float>(rng.generate(0, 1024)),
-                        lua_instance, display.game_renderer.ref());
-
-    for (const auto &slot : proj_spawner.cref())
+  Game::UI::Layout ui {
+    display.game_renderer.ref(),
     {
-      if (enemy_spawner.clearSlot(slot))
+      CastGetVar<std::string>(lua_instance.GetLuaValue(std::array{"UI", "Name", "label"})).c_str(),
+      CastGetVar<int>(lua_instance.GetLuaValue(std::array{"UI", "Name", "x_pos"})),
+      CastGetVar<int>(lua_instance.GetLuaValue(std::array{"UI", "Name", "y_pos"})),
+    },
+    {
+      std::format("{:09}", 0).c_str(),
+      CastGetVar<int>(lua_instance.GetLuaValue(std::array{"UI", "Score", "x_pos"})),
+      CastGetVar<int>(lua_instance.GetLuaValue(std::array{"UI", "Score", "y_pos"})),
+    },
+    {
+      std::format("{:09}", 0).c_str(),
+      CastGetVar<int>(lua_instance.GetLuaValue(std::array{"UI", "HighScore", "x_pos"})),
+      CastGetVar<int>(lua_instance.GetLuaValue(std::array{"UI", "HighScore", "y_pos"})),
+    },
+    {
+      std::format("{:02}", 0).c_str(),
+      CastGetVar<int>(lua_instance.GetLuaValue(std::array{"UI", "TimesPlayed", "x_pos"})),
+      CastGetVar<int>(lua_instance.GetLuaValue(std::array{"UI", "TimesPlayed", "y_pos"})),
+    },
+  };
+
+SDL::Mouse mouse{};
+
+Utils::Rng rng{};
+
+Game::Rules game_rules{CastGetVar<int>(lua_instance.GetLuaValue(std::array{"GameRules", "max_time"})),
+                       CastGetVar<int>(lua_instance.GetLuaValue(std::array{"GameRules", "max_score"}))};
+
+Game::Player player{lua_instance, display.game_renderer.ref()};
+Game::Spawner<Game::Projectile> proj_spawner{
+    [&lua_instance]()
+    {
+      return CastGetVar<int>(lua_instance.GetLuaValue(std::array{"GameRules", "ProjectileSpawner", "max_spawn_slots"}));
+    }};
+Game::Spawner<Game::Enemy> enemy_spawner{};
+
+// Game loop
+while (event_handler.isGameRunning())
+{
+
+  const auto mouse_xy{mouse.GetCursorPosition()};
+  const auto mouse_radian{Utils::Angle::CaclulateAngleBetweenTwoObjectsRadians(
+      mouse_xy, {player.bounds_.cref().x, player.bounds_.cref().y})};
+
+  event_handler.PollEvent(
+      [&]
       {
-        const auto calcNote = [&rng]()
-        {
-          constexpr std::array<float, 3> notes{220.f, 277.18f, 329.63f};
-          const auto rng_result{rng.generate(0, 2)};
-          return notes.at(rng_result);
-        };
+        proj_spawner.spawn(
+            display.game_window.WindowSize(),
+            std::pair<float, float>{display.game_window.WindowSize().first / 2 - 16,
+                                    display.game_window.WindowSize().second / 2 - 16},
+            std::pair<float, float>{
+                Utils::Angle::CalculateXDirection(
+                    mouse_radian, CastGetVar<float>(lua_instance.GetLuaValue(std::array{"Projectile", "speed"}))),
+                Utils::Angle::CalculateYDirection(
+                    mouse_radian, CastGetVar<float>(lua_instance.GetLuaValue(std::array{"Projectile", "speed"})))},
+            lua_instance, display.game_renderer.ref());
+      });
 
-        audio.pushStream(Utils::generateSine(calcNote(), 48000.0f, 48000 / 2, 0.05f));
-        game_rules.score.increase(rng.generate(300, 450), audio);
-        audio.resumeStream();
-      }
-    }
+  proj_spawner.clearSlot();
 
-    // Rendering
-    display.game_renderer.clearScreen();
-    display.game_renderer.drawColorFloat(0, 0, 0);
+  enemy_spawner.spawn(static_cast<float>(rng.generate(0, 1024)), static_cast<float>(rng.generate(0, 1024)),
+                      lua_instance, display.game_renderer.ref());
 
-    display.game_renderer.renderTextureRotate(player.texture_.ref(), nullptr, &player.bounds_.ref(),
-                                              Utils::Angle::CaclulateAngleBetweenTwoObjectsDegree(
-                                                  mouse_xy, {player.bounds_.cref().x, player.bounds_.cref().y}) +
-                                                  Utils::Angle::texture_offset<double>,
-                                              nullptr, SDL_FLIP_NONE);
-    // Spawned Projectile Render:
-    for (auto &projectile : proj_spawner.ref())
+  for (const auto &slot : proj_spawner.cref())
+  {
+    if (enemy_spawner.clearSlot(slot))
     {
-      projectile.bounds_.move();
-      display.game_renderer.renderTexture(projectile.texture_.ref(), nullptr, &projectile.bounds_.ref());
+      const auto calcNote = [&rng]()
+      {
+        constexpr std::array<float, 3> notes{220.f, 277.18f, 329.63f};
+        const auto rng_result{rng.generate(0, 2)};
+        return notes.at(rng_result);
+      };
+
+      audio.pushStream(Utils::generateSine(calcNote(), 48000.0f, 48000 / 2, 0.05f));
+      game_rules.score.increase(rng.generate(300, 450), audio);
+      audio.resumeStream();
     }
-
-    for (auto &enemy : enemy_spawner.ref())
-    {
-      display.game_renderer.renderTexture(enemy.texture_.ref(), nullptr, &enemy.bounds_.ref());
-    }
-
-    player_text.draw(ui.name_xy);
-
-    score_text.swapText(std::to_string(game_rules.score.cref().getCurrent()));
-    score_text.draw(ui.score_xy);
-    high_score_text.swapText(std::to_string(file_data.high_score));
-    high_score_text.draw(ui.high_score_xy);
-    times_played_text.swapText(std::to_string(file_data.times_played));
-    times_played_text.draw(ui.times_played_xy);
-    display.game_renderer.present();
   }
 
-  file_data.high_score = (game_rules.score.cref().getCurrent() > file_data.high_score)
-                             ? game_rules.score.cref().getCurrent()
-                             : file_data.high_score;
-  file_data.times_played += 1;
-  save_file.writeSerialDataToFile(file_data);
+  // Rendering
+  display.game_renderer.clearScreen();
+  display.game_renderer.drawColorFloat(0, 0, 0);
 
-  return 0;
+  display.game_renderer.renderTextureRotate(player.texture_.ref(), nullptr, &player.bounds_.ref(),
+                                            Utils::Angle::CaclulateAngleBetweenTwoObjectsDegree(
+                                                mouse_xy, {player.bounds_.cref().x, player.bounds_.cref().y}) +
+                                                Utils::Angle::texture_offset<double>,
+                                            nullptr, SDL_FLIP_NONE);
+  // Spawned Projectile Render:
+  for (auto &projectile : proj_spawner.ref())
+  {
+    projectile.bounds_.move();
+    display.game_renderer.renderTexture(projectile.texture_.ref(), nullptr, &projectile.bounds_.ref());
+  }
+
+  for (auto &enemy : enemy_spawner.ref())
+  {
+    display.game_renderer.renderTexture(enemy.texture_.ref(), nullptr, &enemy.bounds_.ref());
+  }
+  
+  ui.drawText();
+
+  display.game_renderer.present();
+}
+
+file_data.high_score = (game_rules.score.cref().getCurrent() > file_data.high_score)
+                           ? game_rules.score.cref().getCurrent()
+                           : file_data.high_score;
+file_data.times_played += 1;
+save_file.writeSerialDataToFile(file_data);
+
+return 0;
 }
