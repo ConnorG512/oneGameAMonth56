37dba6427edf244f11178c01aaaae08942e465ce
Connor Garey <connorgarey1999@gmail.com>
2026-02-18 16:08:21 +0000

    Removed unused values.

--- gamescript/player.lua
+++ gamescript/player.lua
@@ -1,8 +1,5 @@
 PlayerValues = {
-  name = "Player",
   sprite = "assets/image/player.png",
-  starting_health = 1000,
-  max_health = 1800,
   bounds_w = 32,
   bounds_h = 32,
   pos_x = 1024 / 2 - 16,
