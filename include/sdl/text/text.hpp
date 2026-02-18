149e3a71cecf0246ade7f93b332d25131e9bca41
Connor Garey <connorgarey1999@gmail.com>
2026-02-18 14:06:33 +0000

    Text now takes xy parameter directly

--- include/sdl/text/text.hpp
+++ include/sdl/text/text.hpp
@@ -13,6 +13,7 @@ namespace SDL
 class Text
 {
   SDL_Renderer &renderer_;
+  std::pair<float, float> xy_{0.0f, 0.0f};
 
   std::unique_ptr<TTF_TextEngine, decltype(&TTF_DestroyRendererTextEngine)> text_engine_{
       TTF_CreateRendererTextEngine(&renderer_), &TTF_DestroyRendererTextEngine};
@@ -24,9 +25,9 @@ class Text
       TTF_CreateText(text_engine_.get(), font_.get(), "Text", 4), &TTF_DestroyText};
 
 public:
-  Text(const std::string&, SDL_Renderer &renderer);
+  Text(const std::string &, std::pair<float, float> xy, SDL_Renderer &renderer);
 
-  auto draw(std::pair<float, float>xy) noexcept -> void;
-  auto swapText(const std::string& text) noexcept -> void;
+  auto draw() noexcept -> void;
+  auto swapText(const std::string &text) noexcept -> void;
 };
 } // namespace SDL
