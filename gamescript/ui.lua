5aa8ade481f4bd3ae6949de5eb4eb2aaa813275e
Connor Garey <connorgarey1999@gmail.com>
2026-02-18 16:05:37 +0000

    Created ui script.

---
+++ gamescript/ui.lua
@@ -0,0 +1,19 @@
+UI = {
+  name = {
+    label = "Player",
+    x_pos = 20,
+    y_pos = 20,
+  },
+  score = {
+    x_pos = 20,
+    y_pos = 60,
+  },
+  high_score = {
+    x_pos = 20,
+    y_pos = 100,
+  },
+  times_played = {
+    x_pos = 20,
+    y_pos = 140,
+  },
+}
