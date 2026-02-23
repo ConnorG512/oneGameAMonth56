1b2b923e889c54b30bdd132c454156bdfbfe27a0
Connor Garey <connorgarey1999@gmail.com>
2026-02-23 15:44:21 +0000

    Restart game method

--- include/game/rules.hpp
+++ include/game/rules.hpp
@@ -3,8 +3,17 @@
 #include "game/counter.hpp"
 #include "game/score.hpp"
 
+// Forward
+namespace Audio
+{
+class Instance;
+} // namespace Audio
+//
+
 namespace Game
 {
+namespace GUI{class Layout;}
+
 class Rules
 {
 public:
@@ -13,5 +22,7 @@ public:
 
   Score score{1000000};
   Counter<int> time{0, 60};
+
+  auto restartGame(Audio::Instance &audio, Game::GUI::Layout &game_ui) -> void;
 };
 } // namespace Game
--- src/game/rules.cpp
+++ src/game/rules.cpp
@@ -1,3 +1,18 @@
 #include "game/rules.hpp"
+#include "game/ui-layout.hpp"
+#include "sdl/audio/audio.hpp"
+#include "sdl/audio/modes.hpp"
+#include <string>
 
 Game::Rules::Rules(int time_start, int time_end, int score_max) : score{score_max}, time{time_start, time_end} {}
+
+auto Game::Rules::restartGame(Audio::Instance &audio, Game::GUI::Layout &game_ui) -> void
+{
+  if (time.isZeroOrLess())
+  {
+    time.reset(Counter<int>::ResetFrom::max);
+    score.reset();
+    audio.playAudio<Audio::Modes::BasicSine<float>>(220.f, 48000.0f, 48000 / 2, 0.05f);
+    game_ui.setText(Game::GUI::Layout::Text::score, std::to_string(score.cref().getCurrent()));
+  }
+}
