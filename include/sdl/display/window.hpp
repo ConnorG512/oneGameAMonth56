2b00027dd6531d697240c89d3fdad6578b7cc569
Connor Garey <connorgarey1999@gmail.com>
2026-02-24 12:20:27 +0000

    Removed OpenGL flag

--- include/sdl/display/window.hpp
+++ include/sdl/display/window.hpp
@@ -10,7 +10,7 @@ namespace SDL
 class Window
 {
   std::unique_ptr<SDL_Window, decltype(&SDL_DestroyWindow)> window_{
-      SDL_CreateWindow("Game Window", 1024, 1024, SDL_WINDOW_OPENGL), &SDL_DestroyWindow};
+      SDL_CreateWindow("Game Window", 1024, 1024), &SDL_DestroyWindow};
 
 public:
   Window() = default;
