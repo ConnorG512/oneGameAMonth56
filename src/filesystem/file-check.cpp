cfe17be1080ceb8afbd82186cbd794d6c1667c7f
Connor Garey <connorgarey1999@gmail.com>
2026-02-24 13:59:04 +0000

    Return the number of files scanned.

--- src/filesystem/file-check.cpp
+++ src/filesystem/file-check.cpp
@@ -5,13 +5,16 @@
 #include <ranges>
 #include <stdexcept>
 
-auto File::validateFiles(const std::span<const char *const> required_files) -> void
+auto File::validateFiles(const std::span<const char *const> required_files) -> int
 {
+  auto scanned_files {0};
   for (const auto &[index, path] : required_files | std::views::enumerate)
   {
     if (!std::filesystem::exists(path))
     {
       throw std::runtime_error(std::format("Failed to find file {}:{}", index, path));
     }
+    scanned_files = index;
   }
+  return scanned_files;
 }
