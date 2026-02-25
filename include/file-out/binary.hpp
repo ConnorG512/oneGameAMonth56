db15cb5383bdc4b04e7aaf22ca2c17380410edc3
Connor Garey <connorgarey1999@gmail.com>
2026-02-25 01:28:22 +0000

    Take in a span instead if T reference.

--- include/file-out/binary.hpp
+++ include/file-out/binary.hpp
@@ -8,19 +8,18 @@ namespace File
 {
 class Binary
 {
-  std::fstream file_{"game.sav", std::ios::out | std::ios::in | std::ios::binary};
+  std::fstream file_;
   std::array<std::uint8_t, 8> magic_{'C', 'G', 'E', 'B', 'I', 'N', 0x00, 0x00};
-
 
 public:
   Binary();
   auto isValidBinary() noexcept -> bool;
 
-  template <typename T> auto writeSerialDataToFile(const T &serial_struct) noexcept -> void
+  template <typename T> auto writeSerialDataToFile(std::span<const T> data) noexcept -> void
   {
     file_.seekp(magic_.size(), std::ios::beg);
 
-    file_.write(reinterpret_cast<const char *>(&serial_struct), sizeof(serial_struct));
+    file_.write(reinterpret_cast<const char *>(data.data()), data.size());
   }
 
   template <typename T> auto readSerialDataFromFile() -> T
