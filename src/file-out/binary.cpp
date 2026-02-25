4b4b9d7b7dbc7a6f2352af86e2230baf0ee78620
Connor Garey <connorgarey1999@gmail.com>
2026-02-25 00:27:37 +0000

    Removed createNewFile

--- src/file-out/binary.cpp
+++ src/file-out/binary.cpp
@@ -1,27 +1,10 @@
 #include "file-out/binary.hpp"
 
 #include <cstring>
-#include <string_view>
-
-namespace 
-{
-  auto createNewFile(std::fstream& file, std::span<uint8_t, 8> magic, std::string_view file_name)
-  {
-    if (!file.is_open())
-    {
-      file.open(file_name.data(), std::ios::out | std::ios::binary);
-
-      file.write(reinterpret_cast<const char *>(magic.data()), magic.size());
-
-      file.close();
-      file.open(file_name.data(), std::ios::in | std::ios::out | std::ios::binary);
-    }
-  }
-}
 
 File::Binary::Binary(const std::string& file_name) : file_{file_name, std::ios::in | std::ios::out | std::ios::binary}
 {
-  createNewFile(file_, magic_, file_name);
+  file_.write(reinterpret_cast<const char *>(magic_.data()), magic_.size());
 }
 
 auto File::Binary::isValidBinary() noexcept -> bool
